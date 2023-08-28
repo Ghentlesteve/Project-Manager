a9c5e70a4592ab167c225243a3fc7694a174e53c
Steve <kwenevsteve@gmail.com>
2023-08-28 11:05:26 +0100

    Project Manager Files

---
+++ data.js
@@ -0,0 +1,23 @@
+const data = [
+    {
+        "columnId": 0,
+        "tasks": [
+            {"taskId": 97522,"content": "Record Javascript Intro"},
+            {"taskId": 7243,"content": "Record Task Manager Project"}
+        ]
+    },
+    {
+        "columnId": 1,
+        "tasks": [
+            {"taskId": 38833,"content": "System Design Notes"}
+        ]
+    },
+    {
+        "columnId": 2,
+        "tasks": [
+            {"taskId": 88198,"content": "DHIS2 Project"},
+            {"taskId": 7243,"content": "Edit OOP Lectures"}
+        ]
+    }
+
+]
